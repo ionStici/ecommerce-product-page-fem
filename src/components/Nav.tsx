91e152d3644868e0e80196ebcdf06a221180f5dd
Ion Stici <ionsticidev@gmail.com>
2023-08-18 12:31:52 +0300

    Finish navigation bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,12 +8,41 @@ import iconHamb from "./../images/icon-menu.svg";
 import iconCart from "./../images/icon-cart.svg";
 
 const Nav = function () {
+  const [navCon, setNavCon] = React.useState(false);
+  const toggleBtn = React.useRef(null);
+  const toggleNav = function ({ target }) {
+    const node = target.nodeName;
+    if (node === "UL" || node === "LI" || node === "A") return;
+
+    const btn = toggleBtn.current;
+    const icon = btn.querySelector("img");
+    const header = btn.closest("header");
+
+    if (!navCon) {
+      icon.src = iconClose;
+      header.classList.add(styles.open);
+
+      setNavCon(true);
+      return;
+    }
+
+    if (navCon) {
+      icon.src = iconHamb;
+      header.classList.remove(styles.open);
+
+      setNavCon(false);
+      return;
+    }
+  };
+
   return (
     <>
       <header className={styles.header}>
         <button
           className={styles.btn_toggle_nav}
           aria-label="Open Navigation Bar"
+          onClick={toggleNav}
+          ref={toggleBtn}
         >
           <img src={iconHamb} alt="" />
         </button>
@@ -22,7 +51,7 @@ const Nav = function () {
           <img src={logo} alt="Sneakers Logo" />
         </a>
 
-        <nav className={styles.nav}>
+        <nav className={styles.nav} onClick={toggleNav}>
           <ul>
             <li>
               <a href=".">Collections</a>
@@ -45,6 +74,7 @@ const Nav = function () {
         <div className={styles.right_corner_box}>
           <button className={styles.btn_cart} aria-label="Open Cart">
             <img src={iconCart} alt="" />
+            {/* <p>3</p> */}
           </button>
 
           <button className={styles.btn_user_photo} aria-label="Profile">
