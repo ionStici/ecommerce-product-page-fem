e54b599da5d593133e55b6b3705aab8790474b80
Ion Stici <ionsticidev@gmail.com>
2023-08-18 11:58:52 +0300

    Update

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -10,8 +10,47 @@ import iconCart from "./../images/icon-cart.svg";
 const Nav = function () {
   return (
     <>
-      <header>
-        <img src="" alt="" />
+      <header className={styles.header}>
+        <button
+          className={styles.btn_toggle_nav}
+          aria-label="Open Navigation Bar"
+        >
+          <img src={iconHamb} alt="" />
+        </button>
+
+        <a className={styles.btn_logo} href=".">
+          <img src={logo} alt="Sneakers Logo" />
+        </a>
+
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <a href=".">Collections</a>
+            </li>
+            <li>
+              <a href=".">Men</a>
+            </li>
+            <li>
+              <a href=".">Women</a>
+            </li>
+            <li>
+              <a href=".">About</a>
+            </li>
+            <li>
+              <a href=".">Contact</a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className={styles.right_corner_box}>
+          <button className={styles.btn_cart} aria-label="Open Cart">
+            <img src={iconCart} alt="" />
+          </button>
+
+          <button className={styles.btn_user_photo} aria-label="Profile">
+            <img src={userPhoto} alt="" />
+          </button>
+        </div>
       </header>
     </>
   );
