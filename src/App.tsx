f32c4750cba0d456cbd43229178edbbc334a24cd
Ion Stici <ionsticidev@gmail.com>
2023-08-20 09:53:07 +0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./styles/index.scss";
 import Nav from "./components/Nav";
 import Slider from "./components/Slider";
+import About from "./components/About";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <main>
         <Nav />
         <Slider />
+        <About />
       </main>
     </>
   );
