0bd14f17cb024042d2942d2641993d1903f1c628
Ion Stici <ionsticidev@gmail.com>
2023-12-04 09:08:06 +0200

    Update

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './../styles/slider.module.scss';
+import { iconNext, iconPrev, iconClose } from '../data/icons';
 
 import prod_1 from './../images/image-product-1.jpg';
 import prod_1_sm from './../images/image-product-1-thumbnail.jpg';
@@ -12,40 +13,6 @@ import prod_3_sm from './../images/image-product-3-thumbnail.jpg';
 
 import prod_4 from './../images/image-product-4.jpg';
 import prod_4_sm from './../images/image-product-4-thumbnail.jpg';
-
-const iconNext = (
-    <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg">
-        <path
-            d="m2 1 8 8-8 8"
-            stroke="#1D2026"
-            strokeWidth="3"
-            fill="none"
-            fillRule="evenodd"
-        />
-    </svg>
-);
-
-const iconPrev = (
-    <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg">
-        <path
-            d="M11 1 3 9l8 8"
-            stroke="#1D2026"
-            strokeWidth="3"
-            fill="none"
-            fillRule="evenodd"
-        />
-    </svg>
-);
-
-const iconClose = (
-    <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg">
-        <path
-            d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
-            fill="#69707D"
-            fillRule="evenodd"
-        />
-    </svg>
-);
 
 const Slider = function (props) {
     const [count, setCount] = React.useState(1);
@@ -163,8 +130,6 @@ const Slider = function (props) {
             btns[+type - 1].classList.add(styles.popup__img_btn_active);
         }
     };
-
-    // // // // // // // // // // // // // // // // // // // //
 
     // // // // // // // // // // // // // // // // // // // //
 
